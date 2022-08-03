dd5d4d6fea2f0692001f0fb37c25d18e20572173
Juicymon <Juicymon>
2022-08-03 19:32:43 -0400

    task 4  Please enter the commit message for your changes. Lines starting

--- pythonBeautifulSoup--master/ouline.py
+++ pythonBeautifulSoup--master/ouline.py
@@ -1,4 +1,6 @@
 #import beautifulsoup and request here
+import requests, json
+from bs4 import BeautifulSoup
 
 
 #function to get job list from url 'https://www.monster.com/jobs/search?q={role}&where={location}'
@@ -17,6 +19,11 @@ def main():
     print("Enter role you want to search")
     role = input()
     # Complete the missing part of this function here
+    print("Enter the location you want to search")
+    location = input()
+
+    ##url = getJobList(role, location)
+    print(f'Role: {role}, Location: {location}')
     
 if __name__ == '__main__':
     main()
